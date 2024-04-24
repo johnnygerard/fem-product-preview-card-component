fd8b335a21dfabff8622ef76ff377e0a56653a27
Johnny Gérard <john@jgerard.dev>
2024-04-24 09:13:11 +0200

    feat: add console logs for production

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  const FONT_SIZE = "font-size: 40px;";
+
+  console.log(
+    "%cFront%cend %cMentor",
+    FONT_SIZE + "color: #67bece;",
+    FONT_SIZE + "color: #3f54a3;",
+    FONT_SIZE +
+      "color: #fff;" +
+      "background-color: #3f54a3;" +
+      "border-radius: 8px;" +
+      "padding: 0 12px;",
+  );
+  console.log("Challenge by Frontend Mentor. Coded by Johnny Gérard.");
+  console.log("Code repository: https://github.com/johnnygerard/fem-product-preview-card-component");
+  console.log("Portfolio: https://www.frontendmentor.io/profile/johnnygerard");
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
